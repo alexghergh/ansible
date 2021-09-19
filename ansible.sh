69a2f7b42c0f085a21e1f4f10239261ee2ce7e85
Alexandru Gherghescu <gherghescu_alex1@yahoo.ro>
2021-09-19 23:39:06 +0300

    Add the dotfiles configs
    Add a local.yml file for ansible.
    
    Add dotfiles configs as tasks.
    
    Add a bootstrap script ansible.sh.

---
+++ ansible.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Author: alexghergh
+
+# get os identification
+if [ -f /etc/os-release ]; then
+    os_id="$(sed '3q;d' /etc/os-release | sed 's/ID=//')"
+else
+    # if we cannot identify the system, just exit safely
+    exit 0
+fi
+
+# update and upgrade the system and install ansible and other dependencies
+
+# if we're on arch linux
+if [ "$os_id" = 'arch' ]; then
+    sudo "$(which pacman)" -Syu git ansible-core --noconfirm
+    "$(which ansible-galaxy)" collection install community.general
+
+# if we're on ubuntu
+elif [ "$os_id" = 'ubuntu' ]; then
+    sudo "$(which apt-get)" update --yes
+    sudo "$(which apt-get)" upgrade --yes
+    sudo "$(which apt-get)" install python-apt python3-apt git ansible-core --yes
+fi
+
+# pull the ansible repo
+"$(which ansible-pull)" --url https://github.com/alexghergh/ansible.git --directory "$HOME"/ansible
